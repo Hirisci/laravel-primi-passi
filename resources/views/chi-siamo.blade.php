2b4560038844d7da8ae8707f9426ed6cffcb7c0a
Hirisci <www.alan92@icloud.com>
2022-07-06 14:02:33 +0200

    first commit

---
+++ resources/views/chi-siamo.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}" >
+</head>
+<body>
+    <header>
+        <ul class="nav-bar">
+            <li><a href="/" >Home</a></li>
+            <li class="active"><a href="/chi-siamo">Chi siamo</a></li>
+            <li><a href="/servizi">Servizi</a></li>
+            <li><a href="/ringraziamenti">Ringiamenti</a></li>
+        </ul>
+    </header>
+    <main>
+        sei nella sezione chi siamo
+    </main>
+</body>
+</html>
